fc9510667849dda58c0b9054b584a0b02b9ec94c
Edgar Irizarry <eirizarry15@yahoo.com>
2019-02-18 21:23:37 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_price_and_description_to_haunted_house.rb
+++ db/migrate/04_add_price_and_description_to_haunted_house.rb
@@ -1,4 +1,4 @@
-class AddPriceAndDescriptionToHauntedHouse < ActiveRecord::Migration
+class AddPriceAndDescriptionToHauntedHouse < ActiveRecord::Migration[4.2]
   def change
     add_column :haunted_houses, :price, :float
     add_column :haunted_houses, :description, :string
